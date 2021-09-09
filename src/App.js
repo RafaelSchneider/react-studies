41a8dfc65889fa9f573dd461da25d59001d6e7d5
Rafael Schneider da Silva <rafael.schneider.s@timwetech.com>
2021-09-09 09:21:53 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ".App.css";
+
+const App = () => {
+  const hello = "Hello World using js variable.";
+  return (
+    <h1>{hello}</h1>
+  )
+};
+
+export default App;
